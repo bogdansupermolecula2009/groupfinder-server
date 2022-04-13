27802d716d7ecddec5c0610b0d74d53c5bbbd809
Bogdan Andrianov <imz-m67@inbox.ru>
2022-04-13 16:15:57 +0300

    init commit

---
+++ src/main/java/ru/heavyproject/groupfinder/server/GroupFinderServerApplication.java
@@ -0,0 +1,13 @@
+package ru.heavyproject.groupfinder.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupFinderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupFinderServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/heavyproject/groupfinder/server/GroupFinderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.heavyproject.groupfinder.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupFinderServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
